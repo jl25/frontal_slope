1374b59889b6728ca6af7f487aea8070c924aea1
jphyswx <jordbenj@mit.edu>
2018-10-16 15:38:36 -0400

    Add files via upload

---
+++ range_bounds.m
@@ -0,0 +1,18 @@
+function [range_bounds,range_val] = range_bounds(A,dim)
+%RANGE_BOUNDS Summary of this function goes here
+
+%% get dim that min/max operated on
+if ~exist('dim','var') || isempty(dim) || sum(isnan(dim)) || isequal(dim,'')
+    minA = min(A);
+    maxA = max(A);
+    cat_dim = find(size(minA) ~= size(A));
+else
+    minA = min(A, [], dim);
+    maxA = max(A, [], dim);
+    cat_dim = dim;
+end
+%% Find the actual returns
+range_bounds = cat(cat_dim, minA, maxA);
+range_val    = range(A,cat_dim);
+end
+
